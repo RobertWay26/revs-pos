b8d712e1abcab868acd4c6f9b411735e2e5f537a
Aiden Stickney <94949106+AidenStickney@users.noreply.github.com>
2024-02-20 23:40:34 -0600

    Create populate_categories.sql

---
+++ sql/populate_categories.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (name, description) VALUES ('Meals', 'Full meal options including a main item with sides.');
+INSERT INTO categories (name, description) VALUES ('Entrees', 'Main dishes featuring a variety of flavors.');
+INSERT INTO categories (name, description) VALUES ('Beverages', 'Drinks ranging from sodas to shakes.');
+INSERT INTO categories (name, description) VALUES ('Sides', 'Side dishes to complement your meal.');
+INSERT INTO categories (name, description) VALUES ('Desserts', 'Sweet treats to finish your meal.');
+INSERT INTO categories (name, description) VALUES ('Supplies', 'Essential items for serving and packaging meals.');
